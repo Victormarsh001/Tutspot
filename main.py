8f974caf48f4978efed49842ede8702d11632088
Victormarsh001 <victorbasy17.3@gmail.com>
2024-07-17 11:18:22 +0000

    Done login and signup
    Implemented sessions, logins and signups

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 
 import sqlite3
 import os
-from flask import Flask, render_template, request, redirect, url_for
+import secrets
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+if not app.secret_key:
+    app.secret_key = secrets.token_urlsafe(32)
+
 if not os.path.exists('tutspotUserData.db'):
    conn = sqlite3.connect('tutspotUserData.db')
    cursor = conn.cursor()
@@ -12,8 +17,6 @@ if not os.path.exists('tutspotUserData.db'):
    cursor.execute("Insert into Data Values('Heung', 'China', 'seongheungshim@gmail.com', 'heung4k')")
    conn.commit()
    conn.close()
-else:
-   print("Database Exists")
             
 
 
@@ -33,7 +36,11 @@ courses = [{'course':"Frontend Web Development", 'language': "HTML • CSS • J
 
 @app.route('/')
 def index():
-  return render_template('index.html', courses=courses, active_user="")
+   if "username" in session:
+      active_user = session["username"]
+   else:
+      active_user = False
+   return render_template('index.html', courses=courses, active_user=active_user)
 
 @app.route('/about')
 def about():
@@ -62,26 +69,55 @@ def signup():
 def terms():
    return render_template('terms.html')
 
-@app.route('/signupDetail', methods=["POST"])
+@app.route('/signupDetail', methods=["GET", "POST"])
 def signupDetail():
-   name = request.form['username']
-   country = request.form['country']
-   email = request.form['email']
-   password = request.form['password']
-   conn = sqlite3.connect('tutspotUserData.db')
-   cursor = conn.cursor()
-   
-   cursor.execute("Insert into Data(name, country, email, password) Values(?,?,?,?)", (name, country, email, password))
-   
-   cursor.execute("select * from Data")
-   user = cursor.fetchall()
-   conn.commit()
-   print(user)
-   conn.close()
-   return render_template('index.html', active_user=name)
+   if request.method == "POST":
+      name = request.form['username']
+      country = request.form['country']
+      email = request.form['email']
+      password = request.form['password']
+      conn = sqlite3.connect('tutspotUserData.db')
+      cursor = conn.cursor()
+      cursor.execute("Select * from Data Where email = :email", {'email': email})
+      user = cursor.fetchone()
+      if user and email in user:
+         conn.close()
+         return "Email already exists. Check your email for your password.", 404
+      else:
+         session["username"] = name
+         session["country"] = country
+         session["email"] = email
+         cursor.execute("Insert into Data(name, country, email, password) Values(:name, :country, :email, :password)", {'name':name, 'country':country, 'email':email, 'password':password})
+         conn.commit()
+         conn.close()
+         return redirect(url_for('index'))
+   else:
+      return render_template('signup.html')
 
 
 
+@app.route('/loginDetail', methods=["GET", "POST"])
+def loginDetail():
+   if request.method == "POST":
+      email = request.form['email']
+      password = request.form['password']
+      conn = sqlite3.connect('tutspotUserData.db')
+      cursor = conn.cursor()
+      cursor.execute("Select * from Data Where email = :email", {'email': email})
+      user = cursor.fetchone()
+      if user and email in user:
+         if password == user[3]:
+            session["username"] = user[0]
+            session["country"] = user[1]
+            session["password"] = password
+            return redirect(url_for('index'))
+         else:
+            return "Incorrect Password", 404
+      else:
+         return "Email does not exist", 404
+   else:
+      return render_template('login.html')
+         
 
 
 if __name__ == '__main__':
